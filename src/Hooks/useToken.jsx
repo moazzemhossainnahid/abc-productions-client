b4c5d829cf8416fa052aa04755e0de00596a0677
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2023-05-03 22:19:04 +0600

    add hooks

---
+++ src/Hooks/useToken.jsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from 'react';
+
+const UseToken = (user) => {
+    const [token, setToken] = useState('');
+    useEffect( () => {
+        const email = user?.user?.email;
+        let today = new Date();
+        let date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+
+        const currentUser = {
+            email:  email,
+            joiningDate: date
+        }
+        if(email){
+            fetch(`https://online-bank-of-bd-server.vercel.app/user/${email}`, {
+                method: 'PUT',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify(currentUser)
+            })
+            .then(res => res.json())
+            .then(data => {
+                const accessToken = data.accessToken;
+                localStorage.setItem('accessToken', accessToken);
+                setToken(accessToken);
+            })
+        }
+
+    }, [user]);
+    return [token];
+};
+
+export default UseToken;
