d966b4fa0182e4d7ad1b50ae0d86475751d9b8e7
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2023-05-11 15:19:23 +0600

    link change

--- src/Components/Posts/Posts.jsx
+++ src/Components/Posts/Posts.jsx
@@ -13,7 +13,7 @@ const Posts = () => {
     const [allBlogs, setAllBlogs] = useState([]);
 
     useEffect(() => {
-        fetch('http://localhost:5000/api/v1/posts')
+        fetch('https://attractive-shrimp.cyclic.app/api/v1/posts')
             .then(res => res.json())
             .then(data => setAllBlogs(data.filter(b => b?.status === 'approve')))
     }, [])
